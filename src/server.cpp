04c29364828bc74e2318d2d49bf890ac57f57dc1
Jonathan WEBSTER <64645223+JWR26@users.noreply.github.com>
2025-07-21 23:33:36 +0200

    send a simple HTTP response

--- src/server.cpp
+++ src/server.cpp
@@ -51,8 +51,11 @@ int main(int argc, char **argv) {
   
   std::cout << "Waiting for a client to connect...\n";
   
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  int socket = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
   std::cout << "Client connected\n";
+  std::string msg{"HTTP/1.1 200 OK\r\n\r\n"};
+
+  size_t bytes = send(socket, msg, msg.size(), 0);
   
   close(server_fd);
 
