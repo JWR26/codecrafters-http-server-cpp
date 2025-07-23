5127257d677eef3d98865d8a211a71f6e9edbc83
Jonathan WEBSTER <64645223+JWR26@users.noreply.github.com>
2025-07-23 21:58:55 +0200

    extract url path

--- src/server.cpp
+++ src/server.cpp
@@ -42,7 +42,7 @@ http_request parse_request(const std::string& str){
   return request;
 }
 
-char[] get_response(const request_line& line){
+void* get_response(const request_line& line){
   if (line.TARGET != ""){
     return "HTTP/1.1 404 Not Found\r\n\r\n";
   }
